bc8dfec9d946814be05e0f0a1069cd3d0869bab9
HazimZaghlol <business333man@gmail.com>
2025-02-20 14:37:47 +0200

    first commit

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,5 +1,30 @@
 export interface Product {
-  product: any;
+  product: {
+    subcategory: {
+      _id: string;
+      name: string;
+      slug: string;
+      category: string;
+    }[];
+    _id: string;
+    title: string;
+    quantity: number;
+    imageCover: string;
+    category: {
+      _id: string;
+      name: string;
+      slug: string;
+      image: string;
+    };
+    brand: {
+      _id: string;
+      name: string;
+      slug: string;
+      image: string;
+    };
+    ratingsAverage: number;
+    id: string;
+  };
   _id: string;
   sold: number;
   title: string;
